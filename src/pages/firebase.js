6cce3d55fb4df1fa81eb3274b60c8154d674b1b6
BhathikaGimhan <bgmaduragoda@gmail.com>
2024-02-13 19:11:05 +0530

    env and firebase update

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC98lPVVgGkpYeaI1H3OpE7pgXtPfJlwhc",
-  authDomain: "ideadrive-pvt-ltd.firebaseapp.com",
-  projectId: "ideadrive-pvt-ltd",
-  storageBucket: "ideadrive-pvt-ltd.appspot.com",
-  messagingSenderId: "323020288865",
-  appId: "1:323020288865:web:d9898441bdeedf9905cb20",
-  measurementId: "G-ZTYRQX190F",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
 export { app };
